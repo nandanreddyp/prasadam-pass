aca92a909fe65500a85d0fed4dbd0d267136ff43
nandyiitm <22f3002857@ds.study.iitm.ac.in>
2025-08-13 03:07:04 +0530

    db url

--- prasadam_pass/settings.py
+++ prasadam_pass/settings.py
@@ -81,12 +81,21 @@ WSGI_APPLICATION = 'prasadam_pass.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import dj_database_url
+
+DATABASE_URL = config("DATABASE_URL", default=None)
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
