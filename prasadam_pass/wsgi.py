ae8e925188da347b204e39d9042328458fcbc871
nandyiitm <22f3002857@ds.study.iitm.ac.in>
2025-08-13 07:51:56 +0530

    final?

--- prasadam_pass/wsgi.py
+++ prasadam_pass/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prasadam_pass.settings')
 
 application = get_wsgi_application()
+
+app = application
